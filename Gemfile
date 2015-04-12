da8bd8bba57a5e0b4209ce9ad4427a0cbc4c17a2
livinghighlight23 <terrenceautry@yahoo.com>
2015-04-12 22:13:42 +0000

    Adding dropbox support ot carrierwave

--- Gemfile
+++ Gemfile
@@ -1,30 +1,18 @@
 source 'https://rubygems.org'
+
 gem 'rails', '4.1.0'
-
 gem 'sass-rails', '~> 4.0.3'
-
 gem 'uglifier', '>= 1.3.0'
-
 gem 'coffee-rails', '~> 4.0.0'
-
 gem 'jquery-rails'
-
 gem 'turbolinks'
-
 gem 'jbuilder', '~> 1.2'
-
 gem 'carrierwave'
-
 gem 'carrierwave-dropbox'
 
-gem "figaro"
-
-gem "mini_magick"
 
 group :production do
-
 gem 'pg'
-
 gem 'rails_12factor'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327035856) do
+ActiveRecord::Schema.define(version: 20150403162221) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150327035856) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
