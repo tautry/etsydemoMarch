85b302b72ced4102f241999c56bce7d27abaf974
livinghighlight23 <terrenceautry@yahoo.com>
2015-04-17 02:40:58 +0000

    change link for banner image to use dropbox

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,14 +2,12 @@ class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
 before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
-  
- 
-  # GET /listings
-  # GET /listings.json
-  
-   def seller 
-     @listing = Listing.where(user: current_user).order("created_at DESC")
+
+  def seller
+     @listings = Listing.where(user: current_user).order("created_at DESC")
   end
+     # GET /listings
+  # GET /listings.json
   def index
     @listings = Listing.all.order("created_at DESC")
   end
@@ -79,6 +77,7 @@ before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destro
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :image)
     end
+
   def check_user
       if current_user != @listing.user
         redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
